94bcf6f622a6866d05e3197a2b4a91511a1a9ffb
Eubert Lloyd <yuuberuu@gmail.com>
2025-12-06 23:02:52 +0800

    update settings

--- campus_marketplace/campus_marketplace/settings.py
+++ campus_marketplace/campus_marketplace/settings.py
@@ -98,11 +98,13 @@ WSGI_APPLICATION = 'campus_marketplace.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://localhost/campus_marketplace',
+        conn_max_age=600,
+        conn_health_checks=True,  # Add this
+    )
 }
 
 
